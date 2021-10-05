5cea4f04657f478b98f9bd77c191b600c44a79e7
JORGE ARMANDO JUAREZ VAZQUEZ <2018371036@uteq.edu.mx>
2021-10-04 21:49:56 -0500

    Agregar archivos de proyecto.

---
+++ App6/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,69 @@
+﻿using App6.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App6.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private string _userName;
+        private string _password;
+        private Command _loginCommand;
+        private bool isErrorLogin;
+
+        public MainPageViewModel(INavigation navigation) : base(navigation)
+        {
+
+        }
+
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                _userName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command LogInCommand
+        {
+            get => _loginCommand ?? (_loginCommand = new Command(GoToLoginAction));
+        }
+
+        public bool IsErrorLogin
+        {
+            get => isErrorLogin;
+            set
+            {
+                isErrorLogin = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private void GoToLoginAction()
+        {
+            IsErrorLogin = !(string.Equals(UserName, "holi") && string.Equals(Password, "Nada"));
+
+            if (!IsErrorLogin)
+            {
+                UserName = String.Empty;
+                Password = String.Empty;
+                Navigation.PushAsync(new PaginaInicio());
+            }
+        }
+    }
+}
